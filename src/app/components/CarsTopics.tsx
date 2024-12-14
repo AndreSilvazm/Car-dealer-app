4ab3cd3b5683965dfcde96f167d08a036ac9db75
AndreSilvazm <andre.mani2015@gmail.com>
2024-12-14 20:22:48 -0300

    :broom: cleanup: Cleaning up code

--- src/app/components/CarsTopics.tsx
+++ src/app/components/CarsTopics.tsx
@@ -6,28 +6,28 @@ import Eletro from '@/app/Midias/EletroImg.jpg';
 import TopicsCard from '@/app/components/UI/Cards/TopicsCard';
 
 function CarTopics() {
-  const CarsDetails = [
-    {
-      imgUrl: Eletro,
-      Title: "Electric Cars",
-    },
-    {
-      imgUrl: SUV,
-      Title: "SUV's",
-    },
-    {
-      imgUrl: Picapes,
-      Title: "Pickups",
-    },
-    {
-      imgUrl: Sedan,
-      Title: "Sedans",
-    },
-    {
-      imgUrl: Hatches,
-      Title: "Hatches",
-    },
-  ];
+const CarsDetails = [
+  {
+    imgUrl: Eletro,
+    Title: "Electric Cars",
+  },
+  {
+    imgUrl: SUV,
+    Title: "SUV's",
+  },
+  {
+    imgUrl: Picapes,
+    Title: "Pickups",
+  },
+  {
+    imgUrl: Sedan,
+    Title: "Sedans",
+  },
+  {
+    imgUrl: Hatches,
+    Title: "Hatches",
+  },
+];
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-5 lg:grid-cols-5 gap-4 px-2">
