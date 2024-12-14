444290e2ee1b947834531a84ea572f123829986e
AndreSilvazm <andre.mani2015@gmail.com>
2024-12-14 20:47:26 -0300

    testing

--- src/app/components/UI/Header/Header.tsx
+++ src/app/components/UI/Header/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Header() {
   return ( 
-    <Link href="/" className="text-black font-semibold h-10 flex flex-col justify-center items-center w-full bg-white">
+    <Link href="#" className="text-black font-semibold h-10 flex flex-col justify-center items-center w-full bg-white">
       Home
     </Link>
   );
